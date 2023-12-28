355835af5a782c2746ef1e9cec16bdcab27ccecd
Paul Sisson <paulsissonsemail@gmail.com>
2023-12-28 14:54:37 -0600

    Add climb stairs solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# counter
+# counter and climbing stairs
+
+- Not even close to the best runtime score but I'm really happy with the fact that I solved these two with zero help.
--- main.js
+++ main.js
@@ -19,18 +19,95 @@
 
 
 /////// first attempt /////////
-var createCounter = function(n) {
-    return function() {
+var createCounter = function (n) {
+  return function () {
     return n++                                    /////had to fiddle around a bit but closure is really hard to understand...
-    };                                            /////I think I'll understand it more when I see a real case of why it matters
+  };                                            /////I think I'll understand it more when I see a real case of why it matters
 };
 
 
-  const counter = createCounter(10)
-  console.log(counter()) // 10
-  console.log(counter()) // 11
-  console.log(counter()) // 12
-  const nextCounter = createCounter(30);
-  console.log(nextCounter())
-  console.log(nextCounter())
-  console.log(nextCounter())
+// const counter = createCounter(10)
+// console.log(counter()) // 10
+// console.log(counter()) // 11
+// console.log(counter()) // 12
+// const nextCounter = createCounter(30);
+// console.log(nextCounter())
+// console.log(nextCounter())
+// console.log(nextCounter())
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+
+// Constraints:
+
+// 1 <= n <= 45
+
+function howManyCombos(n) {
+  let answers = []
+  let maxTwos = Math.floor(n / 2)
+
+  function sum(array) {
+    return array.reduce((acc, cv) => acc + cv, 0)
+  }
+
+  function getOnesAndTwos(array) {
+    return array.reduce((acc, cv) => {
+      cv === 1 ? acc[1] += 1 : acc[2] += 1
+      return acc
+    }, { 1: 0, 2: 0 })
+  }
+
+  function getFactorial(num) {
+    let factorial = num
+    if (num === 0 || num === 1) {
+      return 1
+    }
+    while (num > 1) {
+      num--
+      factorial *= num
+    }
+    return factorial
+  }
+
+  while (maxTwos >= 0) {
+    const currentAnswer = []
+    for (let i = 0; i < maxTwos; i++) {
+      currentAnswer.push(2)
+    }
+    while (sum(currentAnswer) < n) {
+      currentAnswer.push(1)
+    }
+    answers.push(currentAnswer)
+    maxTwos -= 1
+  }
+
+  let totalPossibilities = 0
+  answers.forEach((answer) => {
+    let onesFactorial = getFactorial(getOnesAndTwos(answer)[1])
+    let twosFactorial = getFactorial(getOnesAndTwos(answer)[2])
+    let totalLengthFactorial = getFactorial(answer.length)
+    totalPossibilities += totalLengthFactorial / (onesFactorial * twosFactorial)
+  })
+
+  return totalPossibilities
+}
+
+console.log(countByTwos(8))
